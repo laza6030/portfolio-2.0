180374e0bdd4ade828ac2baa7ac412d457d3eab3
Laza Nantenaina <laza@hairun-technology.com>
2024-09-19 23:11:46 +0300

    Setup + Install some deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import NavBar from "@/app/components/NavBar";
+import IntroduceMe from "@/app/components/IntroduceMe";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center">
+      <NavBar />
+
+      <IntroduceMe />
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    // https://coolors.co/palette/0d1b2a-1b263b-415a77-778da9-e0e1dd
+    colors: {
+      disabled: "#8d99ae",
+      "light-gray": "#e0e1dd",
+      "cadet-blue": "#778DA9",
+      "steel-blue": "#415A77",
+      "gunmetal-blue": "#1B263B",
+      "dark-blue": "#0D1B2A",
     },
   },
   plugins: [],
 };
+
 export default config;
