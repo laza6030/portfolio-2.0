f23d6344b37e76b0a87879995fc76e0eff5b720e
Laza Nantenaina <laza@hairun-technology.com>
2024-09-21 21:40:17 +0300

    Update nav style/ add working link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import NavBar from "@/app/components/NavBar";
+import Footer from "@/app/components/Footer/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-light-gray`}>{children}</body>
+      <body
+        className={`${inter.className} bg-light-gray flex justify-center text-gunmetal-blue`}
+      >
+        <main className="flex flex-col items-center w-3/5 h-screen ">
+          <NavBar />
+          {children}
+          <Footer />
+        </main>
+      </body>
     </html>
   );
 }
