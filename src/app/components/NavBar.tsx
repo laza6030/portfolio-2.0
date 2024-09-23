4af7e05b28c330165e1e6d8edfb59576eac1a378
Laza Nantenaina <laza@hairun-technology.com>
2024-09-23 14:59:09 +0300

    Highlight active link

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,30 +1,52 @@
 "use client";
 
 import { useState } from "react";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { MdSunny } from "react-icons/md";
 import { RiMoonFill } from "react-icons/ri";
 
 export default function NavBar() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const pathname = usePathname();
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   const handleSwitchTheme = () => {
     setIsDarkMode((prev) => !prev);
   };
 
+  const isCurrentPath = (path: string) => {
+    return pathname.includes(path);
+  };
+
   return (
     <div className="flex flex-row justify-between w-full">
       <div className="flex flex-row">
-        <Link href="/" className="flex justify-center p-4">
+        <Link
+          href="/"
+          className={clsx("flex justify-center p-4", {
+            "text-cadet-blue": pathname === "/",
+          })}
+        >
           Home
         </Link>
 
-        <Link href="/projects" className="flex justify-center p-4">
+        <Link
+          href="/projects"
+          className={clsx("flex justify-center p-4", {
+            "text-cadet-blue": isCurrentPath("/projects"),
+          })}
+        >
           Projects
         </Link>
 
-        <Link href="/about" className="flex justify-center p-4">
+        <Link
+          href="/about"
+          className={clsx("flex justify-center p-4", {
+            "text-cadet-blue": isCurrentPath("/about"),
+          })}
+        >
           About
         </Link>
       </div>
