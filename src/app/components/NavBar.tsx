78806a2d42c5952eda9912eda173e90fe7fff56f
Laza Nantenaina <laza@hairun-technology.com>
2024-10-01 11:32:35 +0300

    Try add animation on nav bar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, SetStateAction, Dispatch } from "react";
+import { SetStateAction, Dispatch } from "react";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -31,28 +31,52 @@ export default function NavBar(props: IProps) {
     return pathname.includes(path);
   };
 
-  const menuList: { label: string; path: string; isActive: boolean }[] = [
-    { label: "Home", path: "/", isActive: pathname === "/" },
+  const menuList: {
+    label: string;
+    path: string;
+    isActive: boolean;
+    delay?: string;
+  }[] = [
+    {
+      label: "Home",
+      path: "/",
+      isActive: pathname === "/",
+      delay: "0",
+    },
     {
       label: "Projects",
       path: "/projects",
       isActive: isCurrentPath("/projects"),
+      delay: "200",
     },
-    { label: "About", path: "/about", isActive: isCurrentPath("/about") },
-    { label: "Career", path: "/career", isActive: isCurrentPath("/career") },
+    {
+      label: "About",
+      path: "/about",
+      isActive: isCurrentPath("/about"),
+      delay: "400",
+    },
+    {
+      label: "Career",
+      path: "/career",
+      isActive: isCurrentPath("/career"),
+      delay: "600",
+    },
   ];
 
   return (
     <div className="flex flex-row justify-between w-full">
       <div className="flex flex-row">
-        {menuList.map(({ label, path, isActive }, index) => (
+        {menuList.map(({ label, path, isActive, delay }, index) => (
           <Link
             key={`nav-menu-${index}`}
             href={path}
-            className={clsx("flex justify-center p-4 hover:font-bold w-20", {
-              "text-cadet-blue": isActive,
-              "dark:text-cadet-blue": isActive,
-            })}
+            className={clsx(
+              `flex justify-center p-4 hover:font-bold w-20 animate-fade-down animation-delay-[${delay}ms] relative`,
+              {
+                "text-cadet-blue": isActive,
+                "dark:text-cadet-blue": isActive,
+              }
+            )}
           >
             {label}
           </Link>
