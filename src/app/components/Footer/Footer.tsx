6a27994c02b1b0fe7bf8e4cfc862dfccb5cf0a12
Laza Nantenaina <laza@hairun-technology.com>
2024-10-16 23:34:22 +0300

    Descrease font size

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "./SocialMedia";
 export default function Footer() {
   return (
     <div className="fixed bottom-0 flex w-1/2 flex-col items-center border-t-[1px] bg-light-gray pt-3 transition-bg dark:bg-gunmetal-blue">
-      <p>Reach me out</p>
+      <p className="text-sm">Reach me out</p>
 
       <SocialMedia />
 
